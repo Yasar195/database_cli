d1e92a3be283cfec928131e0df3d3708aa1595ee
Yasar195 <imyasar07@gmail.com>
2025-08-04 21:48:24 +0530

    initilized db and system functions

--- cmds/cmds.go
+++ cmds/cmds.go
@@ -6,9 +6,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var supported_databases = []string{
+	"postgreSQL",
+}
+
 func GetRootCmd() *cobra.Command {
+
 	var rootCmd = &cobra.Command{
-		Use:   "database backup utility",
+		Use:   "dBackup",
 		Short: "A simple cli application that handles database backups",
 		Long:  "This is a command line application that will handle all the database data backup easily and securely",
 		Run: func(cmd *cobra.Command, args []string) {
@@ -16,5 +21,45 @@ func GetRootCmd() *cobra.Command {
 		},
 	}
 
+	rootCmd.AddCommand(getSupportedDatabases())
+	rootCmd.AddCommand(getSystemFunctions())
+
 	return rootCmd
+}
+
+func getSupportedDatabases() *cobra.Command {
+	dbCmd := &cobra.Command{
+		Use:   "db",
+		Short: "Databse related cli utilities",
+	}
+
+	dbCmd.AddCommand(&cobra.Command{
+		Use:   "list",
+		Short: "List's all the supported database engines",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Supported database engines:")
+			for _, db := range supported_databases {
+				fmt.Println(db)
+			}
+		},
+	})
+
+	return dbCmd
+}
+
+func getSystemFunctions() *cobra.Command {
+	systemCmd := &cobra.Command{
+		Use:   "system",
+		Short: "Internal system commands",
+	}
+
+	systemCmd.AddCommand(&cobra.Command{
+		Use:   "update",
+		Short: "System update command",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Checking for updates...")
+		},
+	})
+
+	return systemCmd
 }
